fd4e484f6ff9cada0e3a59ae9f778ad515baa14b
Alex <alex.palermo.ai@gmail.com>
2025-12-06 22:02:15 -0500

    init qt.py

---
+++ src/ricer/tools/qt.py
@@ -0,0 +1,8 @@
+"""
+use kvantum to set qt theme
+- make a theme config in ~/.config/kvantum/<theme_name>
+- update ~/.config/Kvantum/kvantum.kvconfig
+- make sure correct env vars are sourced in ~/.profile
+
+TODO: icons?
+"""
